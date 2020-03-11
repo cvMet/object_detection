90590bf2e7a7fe6c81a66f666f9daa03ecbfd3c7
Rubeli Nico <tarubeli@ihomelab.local>
2020-03-11 10:36:11 +0100

    added modified filehandler

--- include/filehandler.h
+++ include/filehandler.h
@@ -10,7 +10,7 @@
 
 class FileHandler {
 public:
-	int nRows=240, nCols=320;
+
 	void writeToFile(std::string data, string filename) {
 		ofstream myfile;
 		myfile.open(filename);
@@ -18,8 +18,34 @@ public:
 		myfile.close();
 		std::cout << "Writing to File succeeded." << endl;
 	}
-	//from https://thispointer.com/how-to-read-data-from-a-csv-file-in-c/
-	std::vector<std::vector<float>> readCsv(std::string filename)
+
+	std::vector<std::vector<float>> melexis_txt_to_distance_array(std::string filename, int rows, int columns) {
+		std::ifstream file(filename);
+		if (!file.is_open()) {
+			std::cout << "File: " << filename << " could not be read" << std::endl;
+			std::vector<std::vector<float>> a;
+			return a;
+		}
+		std::string line = "";
+		std::vector<std::vector<float>> pointcloud(rows, std::vector<float>(columns, 0));
+		int indRow = 0;
+		std::string word;
+		while (getline(file, line)) {
+			std::stringstream s(line);
+			int indCol = 0;
+			while (std::getline(s, word, ',')) {
+				if (std::stof(word) < 3000) { //we assume that the distance is not greater than 3m
+					pointcloud[(indRow % rows)][indCol] += std::stof(word);
+				}
+				++indCol;
+			}
+			++indRow;
+		}
+		file.close();
+		return pointcloud;
+	}
+
+	std::vector<std::vector<float>> espros_csv_to_distance_array(std::string filename, int rows, int columns)
 	{
 		std::ifstream file(filename);
 		if (!file.is_open()) {
@@ -27,26 +53,20 @@ public:
 			std::vector<std::vector<float>> a;
 			return a;
 		}
-
 		std::string line = "";
-		// Iterate through each line and split the content using delimeter
-
-		std::vector<std::vector<float>> pointcloud(nRows, std::vector<float>(nCols, 0));
+		std::vector<std::vector<float>> pointcloud(rows, std::vector<float>(columns, 0));
 		int indRow = 0;
 		std::string word;
+		//the csv file consists of 25 datasets. Each set consists of three subsets (distance, amplitude, distance filtered). Each subset consists of 240x320pixel.
+		//All unfiltered distance values get accumulated
 		while (getline(file, line)) {
-
-
-			if ((indRow / nRows) % 3 == 0) { // only add raw data -> row 0-239, 720-959 etc
-				// used for breaking words 
+			if ((indRow / rows) % 3 == 0) {
 				std::stringstream s(line);
-
-				// read every column data of a row and 
-				// store it in a string variable, 'word' 
+				// convert string to float and store it in pointcloud
 				int indCol = 0;
 				while (std::getline(s, word, ',')) {
 					if (std::stof(word) < 3000) { //we assume that the distance is not greater than 3m
-						pointcloud[(indRow % nRows)][indCol] += std::stof(word);
+						pointcloud[(indRow % rows)][indCol] += std::stof(word);
 					}
 					++indCol;
 				}
@@ -55,60 +75,15 @@ public:
 		}
 		// Close the File
 		file.close();
-
-		for (int i = 0; i < nRows; ++i) {
-			for (int j = 0; j < nCols; ++j) {
-				pointcloud[i][j] = pointcloud[i][j] /(indRow/nRows);
+		//Divide by number of subsets to get mean <---- BUG (SHOULDNT IT BE 25) ?!
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				pointcloud[i][j] = pointcloud[i][j] /(indRow/rows);
 			}
 		}
 		return pointcloud;
 	}
 
-		pcl::PointCloud<pcl::PointXYZ> getCloud(std::vector<std::vector<float>> cloud, float f, float x_pixel, float y_pixel, float lowerBoundary)
-	{
-		pcl::PointCloud<pcl::PointXYZ> pointCloud;
-		// Fill in the cloud data
-		pointCloud.width = nCols;
-		pointCloud.height = nRows;
-		pointCloud.is_dense = false;
-		pointCloud.points.resize(pointCloud.width * pointCloud.height);
-		int ind = 0;
-		double normalize = 0;
-		for (int i = 0; i < nRows; ++i)
-		{
-			for (int j = 0; j < nCols; ++j)
-			{
-				//make sure the cloud is in m since the csv file is in mm
-				normalize = 0.001 / sqrt(((j - nCols / 2) * x_pixel * (j - nCols / 2) * x_pixel) + ((i - nRows / 2) * y_pixel * (i - nRows / 2) * y_pixel) + (f * f));
-				pointCloud.points[ind].x = normalize * cloud[i][j] * (j - nCols / 2) * x_pixel; //j / 1000.0f;//
-				pointCloud.points[ind].y = normalize * cloud[i][j] * (i - nRows / 2) * y_pixel; //i/1000.0f;//(cloud[i][j] / (1000.0f * f)) * (i - nRows / 2) * y_pixel; //i
-				pointCloud.points[ind].z = normalize * cloud[i][j] * f; //mm
-				++ind;
-			}
-		}
-		//Use Medianfilter with size 13x13
-#if 1
-		pcl::MedianFilter<pcl::PointXYZ> median;
-		median.setInputCloud(pointCloud.makeShared());
-		median.setWindowSize(13);
-		median.applyFilter(pointCloud);
-		std::cout << "Medianfilter size: " << median.getWindowSize() << std::endl;
-#endif
-		
-		pcl::PointCloud<pcl::PointXYZ> finalCloud;
-		for (int i = 0; i < pointCloud.size(); ++i) {
-			if (pointCloud.at(i).z > lowerBoundary) {
-				finalCloud.push_back(pointCloud.at(i));
-			}
-		}
-		std::vector<int> vec;
-		pcl::removeNaNFromPointCloud(finalCloud, finalCloud, vec);
-		return finalCloud;
-	}
-
-
-
-	//from: https://github.com/PointCloudLibrary/pcl/blob/master/tools/png2pcd.cpp
 	pcl::PointCloud<pcl::PointXYZ> getCloudFromPNG(char* filename) {
 		// Load the depth input file
 		vtkSmartPointer<vtkImageData> depthImageData;
