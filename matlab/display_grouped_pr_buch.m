43b39458df720a70d890e7ee52cc30c1c56abbe6
Nico Rubeli <nico.rubeli@hslu.ch>
2020-03-30 08:40:54 +0200

    cleaned PR Graph scripts

--- matlab/display_grouped_pr_buch.m
+++ matlab/display_grouped_pr_buch.m
@@ -9,31 +9,29 @@ folder_list = rdir([rootdir, '\**\*.'], 'regexp(name, ''iss\d'')', true);
 marker_index = 1;
 NOF_colors = 7;
 plot_count = 1;
-x_axis = 0.8;
-y_axis = 0.3;
-stepsize = 1;
-keyword_transformation = 'tran';
-keyword_object = 'WhiteBottle';
-
+keyword_transformation = 'rot';
+keyword_object = 'Gipfeli';
 for folder = 1:length(folder_list)
     NOF_keypoints = 0;
     NNDR = [];
     euclidean_distance = [];
     files = dir(strcat(rootdir,'\', folder_list(folder).name, '\*.csv'));
     save_name = strcat(rootdir,'\', folder_list(folder).name, '\',keyword_object,keyword_transformation,'PR_merged.png');
+    %check which legend (rotation or translation) should be created
+    rot = files(1).name(length(files(1).name)-9);
     %load all matches into one array
     for file = 1:length(files)
         filename = strcat(files(file).folder, '\', files(file).name)
         data = load(filename,'-ascii');
-        %Don't include the data if model=scene case SHOT
-        if(data(length(data),1) == length(data))
-            continue
-        end
-        %Don't include the data if model=scene case FPFH
+        %Don't include the data from where model=scene case FPFH
         if(contains(filename, 'FPFH'))
             if(data(length(data),1) == (length(data)-1))
                 continue
             end
+        end
+        %Don't include the data from where model=scene case SHOT
+        if(data(length(data),1) == length(data))
+            continue
         end
         NOF_keypoints = NOF_keypoints + data(length(data),1);
         support_radius = data(length(data),2);
@@ -46,28 +44,33 @@ for folder = 1:length(folder_list)
     %Sort keypoints by NNDR in ascending order
     [sorted_keypoints, sort_idx] = sort(NNDR, 'ascend');
     sorted_euclidean_distance = euclidean_distance(sort_idx);
+    %Sort keypoints by euclidean ascending order
+    %     [sorted_keypoints, sort_idx] = sort(euclidean_distance, 'ascend');
+    %     sorted_euclidean_distance = sorted_keypoints;
+    keypoints_09 = numel(NNDR(NNDR(:,1)<0.9,:));
+    keypoints_092 = numel(NNDR(NNDR(:,1)<0.925,:));
+    keypoints_095 = numel(NNDR(NNDR(:,1)<0.95,:));
+    keypoints_0975 = numel(NNDR(NNDR(:,1)<0.975,:));
+    keypoints_1 = numel(NNDR);
+    collection = [1, keypoints_09,keypoints_092, keypoints_095,keypoints_0975 keypoints_1];
     %Calculate TP&FP -> derive precision and recall
-    tp = 0;
-    fp = 0;
     precision = [];
     recall = [];
-    for i = 1:length(NNDR)
-        if(sorted_euclidean_distance(i) < support_radius)
-            tp = tp + 1;
-        else
-            fp = fp + 1;
-            %ugly workaround to inore the first 4 values if they should
-            %lead to a PR graph starting with precison = 0
-            if(i == 1 | i == 2| i == 3 | i == 4)
-                fp = 0;
-                continue
+    for element = 1:length(collection)
+        tp = 0;
+        fp = 0;
+        for i = 1:collection(element)
+            if(sorted_euclidean_distance(i) < support_radius)
+                tp = tp + 1;
+            else
+                fp = fp + 1;
+                if(i==1)
+                   fp = 0; 
+                end
             end
         end
-        %Define granularity of the curve by determing step size
-        if(mod(i, stepsize) == 0)
-            precision = [precision tp/(tp+fp)];
-            recall = [recall tp/NOF_keypoints];
-        end
+        precision = [precision tp/(tp+fp)];
+        recall = [recall tp/NOF_keypoints];
     end
     legend_name = strcat(folder_list(folder).name);
     %Visualization
@@ -86,14 +89,14 @@ for folder = 1:length(folder_list)
         if(mod(plot_count-1, NOF_colors) == 0)
             marker_index = marker_index + 1;
         end
-        figure(1);
+        figure(2);
         hold on;
         plot(interpolated_precision,interpolated_recall,'-o','LineWidth',1, 'Displayname', legend_name, 'Marker', lineS{marker_index});
-        axis([0 x_axis 0 y_axis]);
+        axis([0 1 0 0.3]) %make sure axis goes from 0 to 1
         grid on;
         set(gca,'FontSize',14);
         xlabel('1-Precision','fontsize',15);ylabel('Recall','fontsize',15);
-        set(gcf, 'Position',  [100, 100, 1200, 900]);
+        set(gcf, 'Position',  [100, 100, 1200, 900])
         plot_count = plot_count+1;
     end
     
