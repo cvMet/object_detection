d9f827775697544ccb8a2cb1f858db49f7b71f39
Nico Rubeli <nico.rubeli@hslu.ch>
2020-06-16 09:55:59 +0200

    updated depth extraction file to match new dataset structure

--- matlab/extract_depth.m
+++ matlab/extract_depth.m
@@ -2,35 +2,27 @@ clear 'all';
 close 'all';
 
 addpath('..\melexis')
-directory = '..\datasets\20_03_20\picture_streams\WhiteBottle\0\Tran\*';
-background_dir = '..\datasets\20_03_20\picture_streams\background\*';
-saveDir = '..\datasets\20_03_20\raw_depth\muttern_schraeg_gross\0_tran\';
-pictureDir = '..\datasets\20_03_20\images\';
+directory = '..\datasets\l2score_eval\picture_streams\sensorcase_12mm\*';
+background_dir = '..\datasets\l2score_eval\picture_streams\background\*';
+saveDir = '..\datasets\l2score_eval\raw_depth\sensorcase_12mm\';
+pictureDir = '..\datasets\l2score_eval\images\sensorcase_12mm\';
 numToAverage = 40;
 
 folders = dir(directory);
-background_folders = dir(background_dir);
 files = dir(strcat(folders(1).folder, '\', folders(1).name, '\*.bltstream'));
-background_files = dir(strcat(background_folders(1).folder, '\', background_folders(1).name, '\*.bltstream'));
+
 % Read from Melexis Settings
 modulationAmplitude = 40;
-
-fileBgr = dir(strcat(background_files(1).folder, '\', background_files(1).name));
+fileBgr = dir(strcat(files(1).folder, '\', files(1).name));
 [background,ampl_backround, stdRaw_background, stdAmp_background] = EVK75024_VisualizerBLTSTREAM(strcat(fileBgr(1).folder, '\', fileBgr(1).name),numToAverage,modulationAmplitude);
-
 M = uint8(ampl_backround);
 fileSave = strcat(pictureDir, 'background', '.bmp');
 imwrite(M,fileSave);
 
+%Pop Background file
+files(1) = [];
+
 for i0=1:(length(files))
-    % for backgr=1:(length(background_files))
-    
-    %         fileBgr = dir(strcat(background_files(backgr).folder, '\', background_files(backgr).name));
-    %         [background,ampl_backround, stdRaw_background, stdAmp_background] = EVK75024_VisualizerBLTSTREAM(strcat(fileBgr(1).folder, '\', fileBgr(1).name),numToAverage,modulationAmplitude);
-    %         M = uint8(ampl_backround);
-    %         fileSave = strcat(pictureDir, 'background', '.bmp');
-    %         imwrite(M,fileSave);
-    
     fileImg = dir(strcat(files(i0).folder, '\', files(i0).name));
     [distRaw,ampl, stdRaw, stdAmp] = EVK75024_VisualizerBLTSTREAM(strcat(fileImg(1).folder, '\', fileImg(1).name),numToAverage,modulationAmplitude);
     
@@ -47,14 +39,10 @@ for i0=1:(length(files))
     %     set(3, 'Position', Pfull);
     %    fileName = strcat(fileImg(1).folder, '\', fileImg(1).name);
     
-%     M = distRaw;
-%     fileSave = strcat(saveDir, '\Model\', fileImg(1).name(1:end-10), '.txt');
-%     writematrix(M,fileSave);
-%     
-%     B = background;
-%     fileSave = strcat(saveDir, '\Background\', fileImg(1).name(1:end-10), '.txt');
-%     writematrix(B,fileSave);
-    
+    M = distRaw;
+    fileSave = strcat(saveDir, fileImg(1).name(1:end-10), '.txt');
+    writematrix(M,fileSave);
+
     M = uint8(ampl);
     fileSave = strcat(pictureDir, fileImg(1).name(1:end-10), '.bmp');
     imwrite(M,fileSave);
