6597f677d5e5db77ffbdd92231c6660941dd8577
Nico Rubeli <nico.rubeli@hslu.ch>
2020-07-17 16:47:17 +0200

    added handling of scenes with number containing three digits

--- matlab/display_object_based_pr.m
+++ matlab/display_object_based_pr.m
@@ -1,18 +1,43 @@
 clear all;
 x_axis = 1;
 y_axis = 1;
-rootdir = '..\PR\Buch\object_threshold_eval\weizenbroetchen_sequence\';
+rootdir = '..\PR\Buch\object_threshold_eval\mutter_sequence\';
 folder_list = rdir([rootdir, '\**\*.'], 'regexp(name, ''SHOT'')', true);
 files = dir(strcat(folder_list(1).name, '\*.csv'));
-groundtruth_hard = [ones(27,1);zeros(5,1);ones(15,1);zeros(50,1)];
-groundtruth = [ones(15,1);zeros(55,1)];
+%Groundtruths for different sets
+weizenbroetchen_groundtruth_hard = [ones(27,1);zeros(5,1);ones(15,1);zeros(50,1)];
+weizenbroetchen_groundtruth = [ones(42,1);zeros(55,1)];
+weizenbroetchen_groundtruth_easy = [ones(15,1);zeros(55,1)];
+weizenbroetchen_groundtruth_hard_extended = [ones(57,1);zeros(55,1)];
+
+mutter_groundtruth_easy = [ones(15,1);zeros(40,1)];
+mutter_groundtruth = [ones(15,1);zeros(70,1)];
 for file = 1:length(files)
     filename = strcat(files(file).folder, '\', files(file).name);
     data = load(filename,'-ascii');
     NOF_matches = data(:,2);
+    %Case where more than 100 scenes were matched -> need to be sorted in
+    %order for groundtruth to be correct
+    if(length(data)>1)
+        fid = fopen(filename);
+        C = [];
+        for entry = 1:length(data)
+            line1 = fgetl(fid);
+            C = [C;strsplit(line1,',')];
+            C{entry} = str2double(extractBetween(C{entry},7,length(C{entry})))
+            C{entry,2} = str2double(C{entry,2});
+        end
+        fclose (fid);
+        C = cell2mat(C);
+        targetCount = C(:,1);
+        matchCount = C(:,2);
+        [sorted_target, sort_idx] = sort(targetCount, 'ascend');
+        sorted_matches = matchCount(sort_idx);
+        NOF_matches = sorted_matches;
+    end
     %Start with threshold=0 and increment till max#matches
     max_matches = max(NOF_matches);
-    P = sum(groundtruth);
+    P = sum(mutter_groundtruth);
     precision = [];
     recall = [];
     for threshold = 0:(max_matches)
@@ -20,7 +45,7 @@ for file = 1:length(files)
         fp = 0;
         for i = 1:length(NOF_matches)
             if(NOF_matches(i) >= threshold)
-                if(groundtruth(i))
+                if(mutter_groundtruth(i))
                     tp = tp + 1;
                 else
                     fp = fp + 1;
